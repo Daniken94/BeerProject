65190c05968b9c341097290d7570b56fb501cb3a
Kamil Klekotka <kamil.klekotka@gmail.com>
2021-12-30 10:57:54 +0100

    Adding pages for beer types and beers from Punk Api

--- BeerApp/PunkApiDB/urls.py
+++ BeerApp/PunkApiDB/urls.py
@@ -2,8 +2,18 @@ from django.urls import path
 from . import views
 
 
-app_name ='home'
+app_name = 'punkapi'
 
 urlpatterns = [
-    path('', views.list_of_beers_type, name='beers_type'),
+    path('', views.homepage, name='homepage'),
+    path('beers_type/', views.list_of_beers_type, name='beers_type'),
+    path('beers_type/page2', views.list_of_beers_type_page2, name='beers_type_page2'),
+    path('beers_type/page3', views.list_of_beers_type_page3, name='beers_type_page3'),
+    path('beers_type/page4', views.list_of_beers_type_page4, name='beers_type_page4'),
+    path('beers_type/page5', views.list_of_beers_type_page5, name='beers_type_page5'),
+    path('beers/', views.list_of_beers, name='beers'),
+    path('beers/page2', views.list_of_beers_page2, name='beers_page2'),
+    path('beers/page3', views.list_of_beers_page3, name='beers_page3'),
+    path('beers/page4', views.list_of_beers_page4, name='beers_page4'),
+    path('beers/page5', views.list_of_beers_page5, name='beers_page5'),
 ]
--- BeerApp/PunkApiDB/views.py
+++ BeerApp/PunkApiDB/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import View
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 import requests
 
@@ -7,6 +8,9 @@ import requests
 # email - kamil@gmail.com
 # hasło - kamil
 
+def homepage(request):
+    return render(request, "homepage.html")
+
 
 def list_of_beers_type(request):
     url = 'https://api.punkapi.com/v2/beers?per_page=80'
@@ -14,9 +18,69 @@ def list_of_beers_type(request):
     return render(request, "list_of_beers_type.html", {"response": response})
 
 
+def list_of_beers_type_page2(request):
+    url = 'https://api.punkapi.com/v2/beers?page=2&per_page=80'
+    response = requests.get(url).json()
+    return render(request, "list_of_beers_type_page2.html", {"response": response})
+
+
+def list_of_beers_type_page3(request):
+    url = 'https://api.punkapi.com/v2/beers?page=3&per_page=80'
+    response = requests.get(url).json()
+    return render(request, "list_of_beers_type_page3.html", {"response": response})
+
+
+def list_of_beers_type_page4(request):
+    url = 'https://api.punkapi.com/v2/beers?page=4&per_page=80'
+    response = requests.get(url).json()
+    return render(request, "list_of_beers_type_page4.html", {"response": response})
+
+
+def list_of_beers_type_page5(request):
+    url = 'https://api.punkapi.com/v2/beers?page=5&per_page=80'
+    response = requests.get(url).json()
+    return render(request, "list_of_beers_type_page5.html", {"response": response})
+
+
+def list_of_beers(request):
+    url = 'https://api.punkapi.com/v2/beers?per_page=80'
+    response = requests.get(url).json()
+    return render(request, "list_of_beers.html", {"response": response})
+
+
+def list_of_beers_page2(request):
+    url = 'https://api.punkapi.com/v2/beers?page=2&per_page=80'
+    response = requests.get(url).json()
+    return render(request, "list_of_beers_page2.html", {"response": response})
+
+
+def list_of_beers_page3(request):
+    url = 'https://api.punkapi.com/v2/beers?page=3&per_page=80'
+    response = requests.get(url).json()
+    return render(request, "list_of_beers_page3.html", {"response": response})
+
+
+def list_of_beers_page4(request):
+    url = 'https://api.punkapi.com/v2/beers?page=4&per_page=80'
+    response = requests.get(url).json()
+    return render(request, "list_of_beers_page4.html", {"response": response})
+
+
+def list_of_beers_page5(request):
+    url = 'https://api.punkapi.com/v2/beers?page=5&per_page=80'
+    response = requests.get(url).json()
+    return render(request, "list_of_beers_page5.html", {"response": response})
+
+
+class OneBeerView(View):
+    def get(self, request):
+        pass
+
+
+
+
 def one_beer(request):
     id = 1
-
     url = f"https://api.punkapi.com/v2/beers/{id}"
 
     response = requests.get(url).json()
