59bb5d2c4eaa329fa7c5a4abaea46b0f697afc26
Kamil Klekotka <kamil.klekotka@gmail.com>
2022-06-05 11:58:09 +0200

    BeerProject beer creation test

--- BeerApp/BeerProject/tests.py
+++ BeerApp/BeerProject/tests.py
@@ -3,10 +3,12 @@ from .models import Beer, Ingredients, BoilVolume, MashTemp, Fermentation, BeerI
 from django.contrib.auth.models import User
 
 
-# class BeerModelTestCase(TestCase):
-#     def setUp(self):    #uruchomiona metoda przy każdym rozpoczęciu testów
-#         self.beer = Beer(name="Atak Chmielu", tagline="IPA", beer_volume="20", unit="litres", preparation_time="20")
+class BeerModelTestCase(TestCase):
+    def setUp(self):    #uruchomiona metoda przy każdym rozpoczęciu testów
+        self.user = User.objects.create(username="Kamil")
+        self.beer = Beer(name="Atak Chmielu", tagline="IPA", beer_volume="20", unit="litres", 
+                            preparation_time="20", user = self.user)
 
-#     def test_beer_creation(self):
-#         self.beer.save()
-#         self.assertIsNotNone(self.beer.id)
+    def test_beer_creation(self):
+        self.beer.save()
+        self.assertIsNotNone(self.beer.id)
